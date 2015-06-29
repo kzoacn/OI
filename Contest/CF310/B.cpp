cca4c3546f029f08d7234a5dd9d132a9dcc5216d
kzoacn <kzoacn@outlook.com>
2015-06-29 17:58:56 +0800

    cf

--- Contest/CF310/B.cpp
+++ Contest/CF310/B.cpp
@@ -3,54 +3,45 @@ using namespace std;
 const int maxn=2e5+5;
 typedef long long LL;
 struct seg{
-	LL l,r,id;
-	bool operator<(const seg &o)const{
-		return l!=o.l?l<o.l:r<o.r;
-	}
+    LL l,r,id;
+    bool operator<(const seg &o)const{
+        return r!=o.r?r<o.r:l<o.l;
+    }
 }se[maxn],s[maxn];
-pair<LL,LL> a[maxn];
 LL n,m;
 bool bycmp(seg a,seg b){
-	return a.id<b.id;
+    return a.id<b.id;
 }
 bool by(pair<LL,LL> a,pair<LL,LL> b){
-	return a.second<b.second;
+    return a.second<b.second;
 }
+set<pair<LL,int> >S;
 int main(){
-	scanf("%I64d%I64d",&n,&m);
-	for(LL i=1;i<=n;i++)
-		scanf("%I64d%I64d",&s[i].l,&s[i].r);
-	for(LL i=1;i<=m;i++)scanf("%I64d",&a[i].first),a[i].second=i;
-	for(LL i=1;i<n;i++){
-		se[i].l=s[i+1].l-s[i].r;
-		se[i].r=s[i+1].r-s[i].l;
-		se[i].id=i;
-	}sort(se+1,se+n);
-	sort(a+1,a+1+m);
-	LL cur=1;
-	static LL anss[maxn];
-	for(LL i=1;i<n;i++){
-		if(a[cur].first>se[i].r)return puts("No"),0;
-		while(cur<=m&&a[cur].first<se[i].l)cur++;
-		if(cur>m)return puts("No"),0;
-		if(a[cur].first<se[i].l)return puts("No"),0;
-		if(a[cur].first>se[i].r)return puts("No"),0;
-		anss[se[i].id]=a[cur].second;
-		cur++;
-	}
-	sort(se+1,se+n,bycmp);
-	sort(a+1,a+1+m,by);
-	for(LL i=1;i<n;i++){
-		LL l=a[anss[i]].first;
-		if(l<se[i].l||se[i].r<l){
-			exit(1);
-			puts("No");
-			return 0;
-		}
-	}
-	
-	puts("Yes");
-	for(LL i=1;i<n;i++)
-		printf("%I64d%c",anss[i]," \n"[i==n-1]);
-	return 0;
+    scanf("%I64d%I64d",&n,&m);
+    for(LL i=1;i<=n;i++)
+        scanf("%I64d%I64d",&s[i].l,&s[i].r);
+    for(LL i=1;i<=m;i++){
+        LL x;scanf("%I64d",&x);
+        S.insert(make_pair(x,i));
+    }
+    for(LL i=1;i<n;i++){
+        se[i].l=s[i+1].l-s[i].r;
+        se[i].r=s[i+1].r-s[i].l;
+        se[i].id=i;
+    }sort(se+1,se+n);
+    //sort(a+1,a+1+m);
+    static LL anss[maxn];
+    for(int i=1;i<n;i++){
+        set<pair<LL,int> >::iterator it=S.lower_bound(make_pair(se[i].l,-1));
+        if(it==S.end()||it->first>se[i].r){
+            return puts("No"),0;
+        }else{
+            anss[se[i].id]=it->second;
+            S.erase(it);
+        }
+    }
+    puts("Yes");
+    for(LL i=1;i<n;i++)
+        printf("%I64d%c",anss[i]," \n"[i==n-1]);
+    return 0;
 }
--- Contest/CF310/C.cpp
+++ Contest/CF310/C.cpp
@@ -1,71 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=2e5+5;
-int q,n;
-#define ls i<<1
-#define rs i<<1|1
-#define lson i<<1,l,(l+r)/2
-#define rson i<<1|1,(l+r)/2+1,r
-struct sgt{
-	int laz[maxn*4];
-	void build(int i,int l,int r){
-		laz[i]=-1;
-		if(l==r){
-			laz[i]=0;
-			return ;
-		}
-		build(lson);
-		build(rson);
+struct node{
+	int laz;
+	node *l,*r;
+	node(){l=r=0;laz=0;}
+	void pd(){
+		if(laz==0)return;
+		if(!l)l=new node();
+		if(!r)r=new node();
+		l->laz=max(l->laz,laz);
+		r->laz=max(r->laz,laz);
 	}
-	void pd(int i){
-		if(laz[i]==-1)return;
-		laz[ls]=max(laz[ls],laz[i]);
-		laz[rs]=max(laz[rs],laz[i]);
-		laz[i]=-1;
-	}
-	int Q(int i,int l,int r,int ps){
-		if(l==r)return laz[i];
-		pd(i);
-		if(ps<=(l+r)/2)return Q(lson,ps);
-		else return Q(rson,ps);
-	}
-	void C(int i,int l,int r,int l0,int r0,int x){
-		if(l0<=l&&r0>=r)return void(laz[i]=x);
-		if(l0<=(l+r)/2)C(lson,l0,r0,x);
-		if(r0>(l+r)/2)C(rson,l0,r0,x);
-	}
-	int Q(int ps){return Q(1,1,n,ps);}
-	void C(int l,int r,int x){C(1,1,n,l,r,x);}
-}T[2];
-vector<int>M1,M2;
-int qx[maxn],qy[maxn],aim[maxn];
+}*T1=new node(),*T2=new node();
+void C(node *&x,int l,int r,int l0,int r0,int d){
+	if(!x)x=new node();
+	if(l0<=l&&r0>=r){
+		x->laz=max(x->laz,d);
+		return;
+	}x->pd();
+	int mid=(l+r)/2;
+	if(l0<=mid)C(x->l,l,mid,l0,r0,d);
+	if(r0>mid)C(x->r,mid+1,r,l0,r0,d);
+}
+int Q(node *&x,int l,int r,int ps){
+	if(!x)return 0;
+	if(l==r)return x->laz;
+	x->pd();int mid=(l+r)/2;
+	if(ps<=mid)return Q(x->l,l,mid,ps);
+	else return Q(x->r,mid+1,r,ps);
+}
+int n,q;
+map<int,int>M;
 int main(){
-	scanf("%*d%d",&q);
-	for(int i=1;i<=q;i++){
-		scanf("%d%d",&qx[i],&qy[i]);
-		char ty=getchar();
-		while(!isalpha(ty))ty=getchar();
-		aim[i]=ty=='L'?1:2;
-		M1.push_back(qx[i]);
-		M2.push_back(qy[i]);
-	}n=1;
-	sort(M1.begin(),M1.end());
-	sort(M2.begin(),M2.end());
-	M1.erase(unique(M1.begin(),M1.end()),M1.end());
-	M2.erase(unique(M2.begin(),M2.end()),M2.end());
-	n=M1.size();
-	T[0].build(1,1,n);	
-	T[1].build(1,1,n);	
-	for(int i=1;i<=q;i++){
-		int ty=aim[i];
-		if(ty==1){
-			int x=lower_bound(M1.begin(),M1.end(),qx[i])-M1.begin()+1;
-			int y=lower_bound(M2.begin(),M2.end(),qy[i])-M2.begin()+1;
-			int lef=T[1].Q(y);
-			printf("%d\n",qx[i]-lef);
-			T[0].C(lef);
+	scanf("%d%d",&n,&q);
+	while(q--){
+		int x,y;char ty[2];
+		scanf("%d%d%s",&x,&y,ty);
+		if(M[x]){puts("0");continue;}
+		M[x]=1;
+		if(*ty=='U'){
+			int z=Q(T1,1,n,x);	
+			printf("%d\n",y-z);
+			C(T2,1,n,z+1,y,x);
 		}else{
-			
+			int z=Q(T2,1,n,y);
+			printf("%d\n",x-z);
+			C(T1,1,n,z+1,x,y);
 		}
 	}
 	return 0;
